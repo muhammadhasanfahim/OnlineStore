fc8fd6c238e2129b94ded41678f0a2c17d0e89fa
muhammad fahim <training.octbd@gmail.com>
2024-05-15 17:17:17 +0600

    initialCommit

--- src/main/java/com/boostmytool/onlinestore/models/Product.java
+++ src/main/java/com/boostmytool/onlinestore/models/Product.java
@@ -1,13 +1,8 @@
 package com.boostmytool.onlinestore.models;
 
-import java.sql.Date;
+import java.util.Date;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="products")
@@ -16,6 +11,54 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public String getPrice() {
+        return price;
+    }
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Date getCreatedate() {
+        return createdate;
+    }
+    public void setCreatedate(Date createdate) {
+        this.createdate = createdate;
+    }
+    public String getImageFileName() {
+        return imageFileName;
+    }
+    public void setImageFileName(String imageFileName) {
+        this.imageFileName = imageFileName;
+    }
     private String name;
     private String brand;
     private String category;
